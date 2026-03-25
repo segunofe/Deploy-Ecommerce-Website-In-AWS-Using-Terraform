a59fe1cc755642042e672ded93aaf0e8f7d3af14
Segun Ofe <83200055+segunofe@users.noreply.github.com>
2026-03-25 15:33:52 -0700

    Create route-53.tf

---
+++ terraform/route-53.tf
@@ -0,0 +1,12 @@
+# # DNS record pointing to ALB
+# resource "aws_route53_record" "site_domain" {
+#   zone_id = data.aws_route53_zone.hosted_zone.zone_id
+#   name    = var.domain_name
+#   type    = "A"
+
+#   alias {
+#     name                   = aws_lb.application_load_balancer.dns_name
+#     zone_id                = aws_lb.application_load_balancer.zone_id
+#     evaluate_target_health = true
+#   }
+# }
