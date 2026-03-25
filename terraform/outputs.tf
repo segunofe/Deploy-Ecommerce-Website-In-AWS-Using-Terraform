a0a05881fbe1c47fe9105060eb2227a6565b5a2f
Segun Ofe <83200055+segunofe@users.noreply.github.com>
2026-03-25 07:37:36 -0700

    added website url block in outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,9 @@
 output "load_balancer_dns" {
   value = aws_lb.application_load_balancer.dns_name
 }
+
+
+# website URL
+# output "website_url" {
+  # value = join("", ["https://", var.record_name, ".", var.domain_name])
+# }
